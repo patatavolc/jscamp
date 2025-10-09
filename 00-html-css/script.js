401c79792956689ab2b7d3b04fbc7a135a45d7c2
EderMoros3 <edermorosrodero@gmail.com>
2025-10-10 01:08:58 +0200

    feat: creacion de script.js e interactividad

---
+++ 00-html-css/script.js
@@ -0,0 +1,7 @@
+
+const boton = document.querySelector('#boton-importante');
+
+boton.addEventListener('click', function responseClick() {
+    lert('Gracias por inscribirte a la solicitud');
+})
+    
