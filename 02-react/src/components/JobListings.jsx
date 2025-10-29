735c1cf4561079f2d0b5b027d27e49c3001001ff
EderMoros3 <edermorosrodero@gmail.com>
2025-10-29 19:40:28 +0100

    feat: componetizacion de los elementos + paginacion funcional

---
+++ 02-react/src/components/JobListings.jsx
@@ -0,0 +1,12 @@
+export function JobListings() {
+  return (
+    // Si la etiqueta esta vacia es un Fragment
+    <>
+      <h2>Resultados de búsqueda</h2>
+
+      <div className="job-listing">
+
+      </div>
+    </>
+  )
+}
